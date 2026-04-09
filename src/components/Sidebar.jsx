adc46cff7ee7d4c0f518b3cccd4437e008c4feaa
farzaddm <farzad.d.m84@gmail.com>
2026-04-09 15:15:25 +0330

    add drawer

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,58 +1,30 @@
-import {Avatar, Box, Divider, Grid, Tab, Tabs, Typography} from "@mui/material";
-import {
-  HomeRounded,
-  FaceRounded,
-  TextSnippetRounded,
-  ConnectWithoutContactRounded,
-  TerminalRounded, FavoriteRounded, CopyrightRounded
-} from "@mui/icons-material";
-import {grey} from "@mui/material/colors";
+import {Box, Drawer, Fab, Grid} from "@mui/material";
+import {grey, red} from "@mui/material/colors";
+import {useState} from "react";
+import DrawerContent from "./ui/DrawerContent";
+import {MenuRounded} from "@mui/icons-material";
 
 const Sidebar = ({value, handleChange}) => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
-  const tabParams = (index) => {
-    return {
-      id: `sidebar-tab-${index}`,
-      "aria-controls": `tabpanel-${index}`,
-    }
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
   }
+
   return (
       <Grid size={{xs: 0, sm: 0, md: 3, lg: 3, xl: 2}} sx={{backgroundColor: grey[900]}}>
-        <Box sx={{justifyContent: "center", textAlign: "center", mt: 2}}>
-          <Avatar src={require("../assets/avatar.png")} variant="rounded"
-                  sx={{height: 200, width: 200, margin: "0 auto", display: {xs: 'none', md: 'block', lg: 'block'}}}/>
-          <Typography variant="h6" color="whitesmoke">فرزاد دهقان منشادی</Typography>
-          <Typography variant="caption" color="whitesmoke">
-            دانشجوی مهندسی کامپیوتر دانشگاه علم و صنعت ایران
-          </Typography>
-
-          <Divider variant="middle" color={grey[900]}/>
-          <Tabs orientation="vertical" variant="scrollable" scrollButtons="auto" allowScrollButtonsMobile value={value}
-                onChange={handleChange} >
-            <Tab label="خانه" icon={<HomeRounded/>} iconPosition="start" {...tabParams(0)}/>
-            <Tab label="درباره من" icon={<FaceRounded/>} iconPosition="start" {...tabParams(1)}/>
-            <Tab label="رزومه من" icon={<TextSnippetRounded/>} iconPosition="start" {...tabParams(2)}/>
-            <Tab label="نمونه کارها" icon={<TerminalRounded/>} iconPosition="start" {...tabParams(3)}/>
-            <Tab label="ارتباط با من" icon={<ConnectWithoutContactRounded/>} iconPosition="start" {...tabParams(4)}/>
-          </Tabs>
-
-          <Divider variant="middle" color={grey[900]}/>
-          <Box sx={{
-            flexGrow: 1,
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "end",
-            alignItems: "center",
-            height: 100
-          }}>
-            <Typography variant="subtitle2" color="whitesmoke">
-              طراحی شده با{" "} <FavoriteRounded sx={{verticalAlign: "middle", color: "tomato", height: 20}}/>
-            </Typography>
-            <Typography variant="caption" color="whitesmoke" sx={{mt: 2}}>
-              کپی رایت ۱۴۰۵{" "}<CopyrightRounded sx={{verticalAlign: "middle", height: 20}}/>
-            </Typography>
-          </Box>
+        <Box sx={{ display: { xs: "block", sm: "block", md: "none", lg: "none", xl: "none" }}} >
+          <Fab aria-label="sidbar" size="small" sx={{m:2, backgroundColor: red[500]}} onClick={handleDrawerToggle}>
+            <MenuRounded/>
+          </Fab>
         </Box>
+        <DrawerContent value={value} handleChange={handleDrawerToggle}/>
+        <Drawer open={drawerOpen} onClose={handleDrawerToggle} sx={{
+          "& .MuiDrawer-paper": {width: 300},
+          display: {xs: "block", sm: "block", md: "none", lg: "none", xl: "none"},
+        }}>
+          <DrawerContent value={value} handleChange={handleDrawerToggle}/>
+        </Drawer>
       </Grid>
   );
 };
