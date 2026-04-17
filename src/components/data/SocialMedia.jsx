0a72ae23ad662f9aa488f6187f7941c8624e38c3
farzaddm <farzad.d.m84@gmail.com>
2026-04-17 15:25:27 +0330

    add social media icons

---
+++ src/components/data/SocialMedia.jsx
@@ -0,0 +1,14 @@
+import {IconButton} from "@mui/material";
+
+const SocialMedia = ({media}) => {
+  return (
+      <IconButton aria-label={media.label}>
+        <a href={media.url} target="_blank" rel="noopener noreferrer">
+          {media.icon}
+        </a>
+      </IconButton>
+
+  )
+}
+
+export default SocialMedia;
--- src/components/sidebar/SidebarHeader.jsx
+++ src/components/sidebar/SidebarHeader.jsx
@@ -1,25 +1,35 @@
-import {Avatar, Typography} from "@mui/material";
+import {Avatar, Box, Typography} from "@mui/material";
 import avatar from "../../assets/pictures/avatar/coat.jpg";
 import {RandomReveal} from "react-random-reveal";
 import {persianAlphabets} from "../../constants";
 import {useState} from "react";
+import socialMedias from "../../constants/socialMedias";
+import SocialMedia from "../data/SocialMedia";
 
 const SidebarHeader = () => {
   const [start, setStart] = useState(false);
+
   return (
       <>
         <Avatar src={avatar} variant="rounded"
                 sx={{height: 200, width: 200, margin: "0 auto", display: {xs: 'none', md: 'block', lg: 'block'}}}/>
 
         <Typography variant="h6" color="whitesmoke">
-          <RandomReveal isPlaying characters="فرزاد دهقان منشادی" duration={1.5} characterSet={persianAlphabets} onComplete={() => setStart(true)}/>
+          <RandomReveal isPlaying characters="فرزاد دهقان منشادی" duration={1.5} characterSet={persianAlphabets}
+                        onComplete={() => setStart(true)}/>
         </Typography>
 
         {start && (
-          <Typography variant="caption" color="gray">
-          دانشجوی کامپیوتر علم و صنعت ایران
-          </Typography>
-          )}
+            <Typography variant="caption" color="gray">
+              دانشجوی کامپیوتر علم و صنعت ایران
+            </Typography>
+        )}
+
+        <Box component="div" sx={{m: "0 auto", textAlign: "center"}}>
+          {socialMedias.map((media, index) => (
+              <SocialMedia key={index} media={media}/>
+          ))}
+        </Box>
       </>
   )
 }
